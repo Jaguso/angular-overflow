e6a67bc123575a181357e276b701faabe66c5edd
jaguso <jagutzo@gmail.com>
2019-01-22 20:18:10 -0600

    added an input for the answer and a submit button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 const modules = [
   MatToolbarModule,
   MatIconModule,
-  MatCardModule
+  MatCardModule,
+  MatInputModule,
+  MatButtonModule
 ];
 
 @NgModule({
